73b1bba9671759bff173028b125fd9d948890d47
Viet Anh Le <mc.vietanh@gmail.com>
2022-04-20 23:44:33 -0400

    can't fix cors error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+const URL = 'https://mean-final-project-be.herokuapp.com/api/upload';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'mean-angular-node-file-upload';
+export class AppComponent implements OnInit {
+  public uploader: FileUploader = new FileUploader({
+    url: URL,
+    allowedFileType: ['pdf', 'image', 'video'],
+    headers: [
+      { name: 'Access-Control-Allow-Origin', value: '*' },
+      {
+        name: 'Content-Type',
+        value:
+          'application/json; application/x-www-form-urlencoded; charset=UTF-8',
+      },
+    ],
+  });
+  ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onCompleteItem = (item: any, status: any) => {
+      console.log('Uploaded File Details:', item, status);
+    };
+  }
+  ngAfterViewInit() {
+    this.uploader.onAfterAddingFile = (item) => {
+      item.withCredentials = false;
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { FileUploadModule } from 'ng2-file-upload';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, // required animations module
+    FormsModule,
+    FileUploadModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
